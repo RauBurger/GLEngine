d909e1d8ce8e1cd72c80f98a92cc8ed2ae28e978
Rob Rau <rfr2@mac.com>
2013-02-20 21:43:58 -0500

    Got it to build under linux. still no actuall support for linux though, just a cmake build system

---
+++ QtGLEngine/src/GLX/GLXRenderWindow.cpp
@@ -0,0 +1,34 @@
+/*
+ *  GLXRenderWindow.cpp
+ *
+ *  Created by Rob Rau on 2/20/13.
+ *  Copyright 2013 Rob Rau. All rights reserved.
+ *
+ */
+
+#include "GLXRenderWindow.h"
+
+GLXRenderWindow::GLXRenderWindow()
+{
+
+}
+
+GLXRenderWindow::GLXRenderWindow(string _name, int _w, int _h, bool _fullScreen)
+{
+
+}
+
+GLXRenderWindow::~GLXRenderWindow()
+{
+
+}
+
+void GLXRenderWindow::_start()
+{
+
+}
+
+void GLXRenderWindow::setInputHandler(InputHandler* inputHandler)
+{
+
+}
